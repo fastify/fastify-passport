b251ccdf306d877b6dba25b225a5f728fff5ebe0
pandaniell <daniell.wdb@icloud.com>
2021-01-06 13:38:13 +0100

    Change from addHook to decoreateRequest

--- src/CreateInitializePlugin.ts
+++ src/CreateInitializePlugin.ts
@@ -6,25 +6,43 @@ import flash = require('fastify-flash')
 export function CreateInitializePlugin(passport: Authenticator, options: { userProperty?: string } = {}) {
   return fp(async (fastify) => {
     void fastify.register(flash)
-    fastify.decorateRequest('passport', null)
-    fastify.decorateRequest('logIn', null)
-    fastify.decorateRequest('login', null)
-    fastify.decorateRequest('logOut', null)
-    fastify.decorateRequest('logout', null)
-    fastify.decorateRequest('isAuthenticated', null)
-    fastify.decorateRequest('isUnauthenticated', null)
-    fastify.decorateRequest(options.userProperty || 'user', null)
 
-    fastify.addHook('onRequest', (request, _reply, done) => {
-      request.logIn = logIn
-      request.login = logIn
-      request.logOut = logOut
-      request.logout = logOut
-      request.isAuthenticated = isAuthenticated
-      request.isUnauthenticated = isUnauthenticated
-      request.passport = passport
-
-      done()
-    })
+    fastify
+      .decorateRequest(options.userProperty || 'user', null)
+      .decorateRequest('passport', {
+        getter() {
+          return passport
+        },
+      })
+      .decorateRequest('logIn', {
+        getter() {
+          return logIn
+        },
+      })
+      .decorateRequest('login', {
+        getter() {
+          return logIn
+        },
+      })
+      .decorateRequest('logOut', {
+        getter() {
+          return logOut
+        },
+      })
+      .decorateRequest('logout', {
+        getter() {
+          return logOut
+        },
+      })
+      .decorateRequest('isAuthenticated', {
+        getter() {
+          return isAuthenticated
+        },
+      })
+      .decorateRequest('isUnauthenticated', {
+        getter() {
+          return isUnauthenticated
+        },
+      })
   })
 }
