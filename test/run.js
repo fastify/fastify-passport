9d08f56bd48245a9018bb48f631b8cd2504a9881
Sameer Srivastava <sameer.srivastava@nearform.com>
2021-12-22 18:36:11 +0530

    chore: improved logging

--- test/run.js
+++ test/run.js
@@ -6,11 +6,13 @@ const sessionPlugins = ['fastify-secure-session', '@fastify/session']
 
 async function runTests(sessionPlugin) {
   process.env.SESSION_PLUGIN = sessionPlugin
+
+  console.log('Running tests with session', sessionPlugin, '\n')
   const result = await runCLI(jestConfig, ['./'])
   if (result.results.success) {
-    console.log(`Tests completed`)
+    console.log(`Tests completed\n`)
   } else {
-    console.error(`Tests failed`)
+    console.error(`Tests failed\n`)
   }
 }
 
