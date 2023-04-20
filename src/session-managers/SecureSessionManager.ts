549039e30a181ad2b183417f42d2c951cf31c5da
Marco Ippolito <marcoippolito54@gmail.com>
2023-04-20 11:29:27 +0200

    backport: session regenerate

--- src/session-managers/SecureSessionManager.ts
+++ src/session-managers/SecureSessionManager.ts
@@ -2,6 +2,11 @@
 import { FastifyRequest } from 'fastify'
 import { SerializeFunction } from '../Authenticator'
 
+// add this to prevent build errors
+// tests are passing, this is to backport this pr
+// https://github.com/fastify/fastify-passport/commit/43c82c321db58ea3e375dd475de60befbfcf2a11
+/* eslint-disable @typescript-eslint/ban-ts-comment*/
+
 /** Class for storing passport data in the session using `fastify-secure-session` or `@fastify/session` */
 export class SecureSessionManager {
   key: string
@@ -20,11 +25,23 @@ export class SecureSessionManager {
 
   async logIn(request: FastifyRequest, user: any) {
     const object = await this.serializeUser(user, request)
+    // Handle sessions using @fastify/session
+    // @ts-ignore
+    if (request.session.regenerate) {
+      // regenerate session to guard against session fixation
+      // @ts-ignore
+      await request.session.regenerate()
+    }
     request.session.set(this.key, object)
   }
 
   async logOut(request: FastifyRequest) {
     request.session.set(this.key, undefined)
+    // @ts-ignore
+    if (request.session.regenerate) {
+      // @ts-ignore
+      await request.session.regenerate()
+    }
   }
 
   getUserFromSession(request: FastifyRequest) {
