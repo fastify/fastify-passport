2dee12b87ebea25d6034c51d47f6402cabc834da
Matteo Collina <hello@matteocollina.com>
2023-07-03 11:59:26 +0200

    Bump typescript dependency (#904)
    * Bump typescript dependency
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    * Update package.json
    
    Co-authored-by: Pelle Wessman <pelle@kodfabrik.se>
    
    ---------
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    Co-authored-by: Pelle Wessman <pelle@kodfabrik.se>

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["/build/", "/node_modules/"],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json'
-    }
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.test.json',
+    }]
   },
   moduleNameMapper: {
     'tiny-lru': require.resolve('tiny-lru')
