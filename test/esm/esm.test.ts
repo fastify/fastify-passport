49bfa0e3ac198d692914363b56d52b1c7b416e41
Aras Abbasi <aras.abbasi@googlemail.com>
2023-10-24 16:43:51 +0200

    remove semver (#1019)

--- test/esm/esm.test.ts
+++ test/esm/esm.test.ts
@@ -1,18 +1,14 @@
 import { spawnSync } from 'child_process'
-import * as semver from 'semver'
 import { join } from 'path'
 
 describe('Native ESM import', () => {
-  const defaultExportTest = semver.lt(process.versions.node, '13.3.0') ? it.skip : it
-  const namedExportTest = semver.lt(process.versions.node, '14.13.0') ? it.skip : it
-
-  defaultExportTest('should be able to use default export', () => {
+  it('should be able to use default export', () => {
     const { status } = spawnSync('node', [join(__dirname, 'default-esm-export.mjs')])
 
     expect(status).toBe(0)
   })
 
-  namedExportTest('should be able to use named export', () => {
+  it('should be able to use named export', () => {
     const { status } = spawnSync('node', [join(__dirname, 'named-esm-export.mjs')])
 
     expect(status).toBe(0)
