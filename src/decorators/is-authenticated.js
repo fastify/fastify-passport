4103bbf0541e9de7c8e1de0805d5650666f4e5cb
Matteo Collina <hello@matteocollina.com>
2024-09-06 15:57:54 +0200

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- src/decorators/is-authenticated.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isAuthenticated = isAuthenticated;
-function isAuthenticated() {
-    var property = this.passport.userProperty;
-    return this[property] ? true : false;
-}
--- src/decorators/is-unauthenticated.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isUnauthenticated = isUnauthenticated;
-function isUnauthenticated() {
-    return !this.isAuthenticated();
-}
